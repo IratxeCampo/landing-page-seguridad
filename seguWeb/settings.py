cfb2040b577017714abdbc2b747f0bdac3f5063c
eguiwow <ander.eguiluz@opendeusto.es>
2021-03-16 18:25:02 +0100

    Debug = False

--- seguWeb/settings.py
+++ seguWeb/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+$mdmi+*(eutri!9^*8!asol-u6+gq@&l1ej#gj1p6e25a52@$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "192.168.1.131", "localhost"]
 
